c4e60ce49c8e02c0bb2684f2bc5e50ebdf3452e5
AntoPrakashURK21CS1168 <110010968+AntoPrakashURK21CS1168@users.noreply.github.com>
2022-09-06 12:19:29 +0530

    Anto

--- Main.py
+++ Main.py
@@ -32,10 +32,10 @@ class Stack:
       while(temp.next != None):
         print(temp.data, end = "=>")
         temp = temp.next
-        else:
-          print(temp.data,"None",sep"=>")
-          else:
-            print("None")
+      else:
+        print(temp.data,"None",sep"=>")
+     else:
+       print("None")
           
 # Do not change the following code
 stack = Stack()
